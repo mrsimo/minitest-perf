fd18d9b5e5e2756210f7023feef06b20f10a1658
Albert Llop <mrsimo@gmail.com>
2013-05-20 00:01:41 +0200

    Basic Test class test

---
+++ test/perf/test_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+module MiniTest::Perf
+  class TestTest < MiniTest::Unit::TestCase
+    def test_creation
+      test = Test.new('run', 'suite', 'name', 10)
+
+      assert_equal 'run',   test.run
+      assert_equal 'suite', test.suite
+      assert_equal 'name',  test.name
+      assert_equal 10,      test.total
+    end
+
+    def test_persists_calls_persistence
+      test = Test.new('run', 'suite', 'name', 10)
+
+      Persistence.expects(:write).with(test)
+
+      test.persist
+    end
+  end
+end
