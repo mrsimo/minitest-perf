e605f00f51a8b7d410f3dc2e2fe5457e4759f703
Albert Llop <mrsimo@gmail.com>
2013-05-21 09:39:58 +0200

    1s = 1000ms

--- lib/minitest/perf/cli.rb
+++ lib/minitest/perf/cli.rb
@@ -5,7 +5,7 @@ module MiniTest
         puts "Slowest individual tests"
         puts
         Statistics.slowest_tests.each do |suite, test_name, time|
-          printf "% 12.2fms | %s#%s\n", time * 100, suite, test_name
+          printf "% 12.2fms | %s#%s\n", time * 1000, suite, test_name
         end
 
         puts
@@ -13,7 +13,7 @@ module MiniTest
         puts "Slowest test suites"
         puts
         Statistics.slowest_suites.each do |suite_name, tests_count, avg_test_time|
-          printf "% 12.2fms | % 4i | %s\n", avg_test_time * 100, tests_count, suite_name
+          printf "% 12.2fms | % 4i | %s\n", avg_test_time * 1000, tests_count, suite_name
         end
       end
     end
